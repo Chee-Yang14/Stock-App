062946c1585d2ea392384356805186c13878e7dd
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2024-02-08 17:02:03 -0600

    added webpack

---
+++ js/auth.js
@@ -0,0 +1,30 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+//sign up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    //get user info
+
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    console.log(email, password);
+
+    // sign up the user
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+})
