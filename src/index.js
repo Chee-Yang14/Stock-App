77ae27f0b208819fac00e8acdc6deb96f5420b62
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2024-02-15 15:02:48 -0600

    updated the menu

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ document.addEventListener('DOMContentLoaded', function(){
     M.Modal.init(modals);
 })
 
+window.onload = function () {
+const menu_btn = document.querySelector('.menu');
+	const mobile_menu = document.querySelector('.mobile-nav');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		mobile_menu.classList.toggle('is-active');
+	});
+}
+
 // $(document).ready(function(){
 //     $('.modal').modal();
 //   });
